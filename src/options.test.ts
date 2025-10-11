72e0eaf3990299dc3bd8e803c1b67c1dbfae045e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-10-11 08:52:59 +0100

    - updated clipboardy now supports wayland

--- src/options.test.ts
+++ src/options.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { parseOptions } from "./options.js";
 
 test("Option parser returns correct args", () => {
-    expect(parseOptions(['--nothing'])).toEqual({ NO_COPY: false, UPDATE: false });
+    expect(parseOptions(['--nothing'])).toEqual({ NO_COPY: false, UPDATE: false, NO_OPEN: false });
     expect(parseOptions(['--n'], { NO: { opts: ['--n'] } })).toEqual({ NO: true });
     expect(parseOptions(['--x'], { NO: { opts: ['--n'] } })).toEqual({ NO: false });
     expect(parseOptions(['--x', '--banana'], { NO: { opts: ['--n', '--banana'] } })).toEqual({ NO: true });
